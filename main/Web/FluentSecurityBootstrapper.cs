ce7d19093b82fff48d1f2d8dcfa44f7e8493d5be
Fabrice MARIANADIN <marianadinf@hotmail.com>
2012-09-04 00:04:40 +0100

    beginning of fluentsecurity bit to manage access rights

--- main/Web/FluentSecurityBootstrapper.cs
+++ main/Web/FluentSecurityBootstrapper.cs
@@ -5,11 +5,14 @@ using System.Web;
 using UIT.iDeal.Infrastructure.Bootstrapper.StartupTasks;
 using FluentSecurity;
 using System.Web.Mvc;
+using UIT.iDeal.Common.Interfaces.Data;
 
 namespace UIT.iDeal.Web
 {
     public class FluentSecurityBootstrapper : IRunTaskAtStartup
     {
+
+        private readonly IUserQuery _query;
 
         public void Execute()
         {
@@ -17,14 +20,16 @@ namespace UIT.iDeal.Web
             {
                 // 1. Use the User Query to get the current user from DB
                 //UserEFContainer dbcontext = new UserEFContainer();
-                
+                var CurrUser = _query.GetOne(user => user.Username == HttpContext.Current.User.Identity.Name);
+
                 //2. Check whether the current user is authenticated
                 // Let Fluent Security know how to get the authentication status of the current user
                 configuration.GetAuthenticationStatusFrom(() => HttpContext.Current.User.Identity.IsAuthenticated);
                 
                 //3. Get its roles , NOTE: it won t be application role but the list of application permission 
                 //var LstRoles = dbcontext.Users.Single(x => x.Id == 1).Roles.ToList().Select(x => x.RoleName);
-                //  configuration.GetRolesFrom(() => dbcontext.Users.Single(x => x.Loginname == HttpContext.Current.User.Identity.Name).Roles.ToList().Select(x => x.RoleName));
+                 //configuration.GetRolesFrom(() => dbcontext.Users.Single(x => x.Loginname == HttpContext.Current.User.Identity.Name).Roles.ToList().Select(x => x.RoleName));
+                configuration.GetRolesFrom(() => CurrUser.ApplicationRoles.ToArray());
 
                 // This is where you set up the policies you want Fluent Security to enforce on your controllers and actions
                 configuration.ForAllControllers().DenyAnonymousAccess();
@@ -33,6 +38,11 @@ namespace UIT.iDeal.Web
             });
 
             GlobalFilters.Filters.Add(new HandleSecurityAttribute(), 0);
+        }
+
+        public FluentSecurityBootstrapper(IUserQuery query )
+        {
+            _query = query;
         }
     }
 }
