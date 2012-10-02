1659fe9bacb061d396f841e8f3b70cf046e6eef3
Franck Theolade <ftheolade@gmail.com>
2012-10-02 09:08:17 +0100

    Included missing ModuleReferenceDataBuilder

--- main/Data.EntityFrameworkProvider/Database/Initialisation/DataContextReferenceDataInitialiser.cs
+++ main/Data.EntityFrameworkProvider/Database/Initialisation/DataContextReferenceDataInitialiser.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
+
 using UIT.iDeal.Common.Builders.Entities;
+
 using UIT.iDeal.Data.EntityFrameworkProvider.Context;
 using UIT.iDeal.Data.EntityFrameworkProvider.Repositories;
+
 using UIT.iDeal.Domain.Model.ReferenceData;
 
 namespace UIT.iDeal.Data.EntityFrameworkProvider.Database.Initialisation
@@ -20,9 +23,7 @@ namespace UIT.iDeal.Data.EntityFrameworkProvider.Database.Initialisation
         private IEnumerable<TReferenceData> GenerateAndSaveReferenceDataFor<TReferenceData>(DataContext context)
             where TReferenceData : ReferenceData, new()
         {
-            return
-                new Repository<TReferenceData>(context)
-                    .SaveList(new ReferenceDataBuilderFor<TReferenceData>());
+            return new Repository<TReferenceData>(context).SaveList(new ReferenceDataBuilderFor<TReferenceData>());
         }
 
         private void GenerateAndSaveReferenceDataFor<TReferenceData>(DataContext context, List<TReferenceData> referenceDatas)
