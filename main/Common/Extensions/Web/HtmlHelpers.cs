7335463eaee777a143741a76a90809aa87031f7f
Franck Theolade <ftheolade@gmail.com>
2012-10-28 18:53:03 +0000

    Helper for application message handling

--- main/Common/Extensions/Web/HtmlHelpers.cs
+++ main/Common/Extensions/Web/HtmlHelpers.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿
+
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Text;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
+using UIT.iDeal.Common.Errors;
 
 namespace UIT.iDeal.Common.Extensions.Web
 {
@@ -108,7 +112,73 @@ namespace UIT.iDeal.Common.Extensions.Web
                                                  classes,
                                                  ExpressionHelper.GetExpressionText(readListExpressionSelector),
                                                  innerHtml) );
-         }
+        }
+
+        /// <summary>
+        /// Generates Application Messages
+        /// </summary>
+        /// <param name="htmlHelper"></param>
+        /// <param name="applicationMessage"> </param>
+        /// <returns>HTML for Application Message</returns>
+        public static string ApplicationMessage(this HtmlHelper htmlHelper, ExecutionResult applicationMessage, string classes = "")
+        {
+            var sb = new StringBuilder();
+            if (applicationMessage != null)
+            {
+                var categories =
+                    EnumExtensions
+                        .GetEnumItems<MessageCategory>()
+                        .Where(x => x != MessageCategory.FatalException ||
+                                    x != MessageCategory.AuthorisationException);
+
+                foreach (var category in categories)
+                {
+                    var group = (category == MessageCategory.BrokenBusinessRule) ?
+                        applicationMessage.ErrorsMessageGroup : applicationMessage.GetMessageGroup(category);
+                    var noItems = !group.Messages.Any();
+
+                    var container = new TagBuilder("div");
+                    var head = new TagBuilder("div");
+                    var content = new TagBuilder("div");
+                    var button = new TagBuilder("div");
+
+                    container.AddCssClass("infobox");
+                    if (noItems) container.AddCssClass("ajax-infobox");
+                    if (classes != string.Empty)
+                    {
+                        container.AddCssClass(classes);
+                    }
+                    container.Attributes.Add("style", "display:none;");
+                    container.Attributes.Add("id", category.ToDescription());
+                    container.AddCssClass(string.Format("bghead_{0}", category.ToDescription()));
+                    container.AddCssClass(string.Format("bgcnt_{0}", category.ToDescription()));
+                    head.AddCssClass("head");
+                    head.InnerHtml = group.PropertyName;
+                    if (category == MessageCategory.Success)
+                    {
+                        var closebtn = new TagBuilder("div");
+                        closebtn.AddCssClass("btn_close");
+                        closebtn.MergeAttribute("title", "close");
+                        head.InnerHtml += closebtn.ToString();
+                    }
+                    content.AddCssClass("content");
+                    button.AddCssClass("button");
+                    button.AddCssClass(string.Format("btn_{0}", category.ToDescription()));
+
+                    var itemSB = new StringBuilder("<ul>");
+                    foreach (var message in group.Messages)
+                    {
+                        itemSB.Append(string.Format("<li>{0}</li>", message));
+                    }
+                    itemSB.Append("</ul>");
+                    content.InnerHtml = itemSB.ToString();
+                    container.InnerHtml = string.Format("{0}\n{1}\n{2}\n", head.ToString(), content.ToString(), button.ToString());
+                    sb.Append(container.ToString());
+                }
+                
+            }
+            return sb.ToString();
+        }
 
     }
 }
---
+++ main/ViewModel/BaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIT.iDeal.Common.Errors;
+
+namespace UIT.iDeal.ViewModel
+{
+    public class BaseViewModel
+    {
+        private ExecutionResult ApplicationMessage { get; set; }
+    }
+}
